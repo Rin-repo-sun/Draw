456118ba6a1c2b4483f1c175b967aa5bd2ce89ac
Rin-repo-sun <wwoodcool@gmail.com>
2022-02-18 23:46:47 +0900

    add line

---
+++ line.js
@@ -0,0 +1,26 @@
+function setup(){	//First to be executed
+//Declaration to start drawing
+	ctx.beginPath();
+//Move the pen position to (200, 100)
+	ctx.moveTo(200, 100);
+//Draw a line from the pen's current position (200, 100) to (800, 900)
+	ctx.lineTo(800, 900);
+//Draw a line on the path where the pen has moved.
+	ctx.stroke();
+}
+
+function loop(){	//Always executed
+	
+}
+
+function touchStart(){	//When touched (mouse down)
+	
+}
+
+function touchMove(){	//When a finger is moved (mouse is moved)
+	
+}
+
+function touchEnd(){	//When finger is released (mouse up)
+	
+}
