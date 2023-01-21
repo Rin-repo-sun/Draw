5df46270350b559fbf636e03d4c2a586f6f7f9d1
root <root@DESKTOP-MQEII7L>
2023-01-21 11:01:49 +0900

    add base

---
+++ base.js
@@ -0,0 +1,31 @@
+function setup() { //First to be executed
+}
+
+function loop() { //Always executed
+	for (let i=0; i < 360; i += 3) {
+		let kakudo = i / 180 * Math.PI;
+
+		let hankei1 = 400;
+
+		let hankei2 = 10;
+
+		let x = hankei1 * Math.cos(kakudo) + screenWidth / 2;
+		let y = hankei1 * Math.sin(kakudo) + screenHeight / 2;
+
+		ctx.beginPath();
+		ctx.arc(x, y, hankei2, 0, Math.PI * 2, true);
+		ctx.stroke();
+	}
+}
+
+function touchStart() { //タッチ(マウスダウン)されたら
+
+}
+
+function touchMove() { //指が動いたら(マウスが動いたら)
+
+}
+
+function touchEnd() { //指が離されたら(マウスアップ)
+
+}
