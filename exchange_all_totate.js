eb28f7f4d4c245bb6392f962197a3680aebd253b
root <root@DESKTOP-MQEII7L>
2023-02-08 08:47:30 +0900

    add rotate action

---
+++ exchange_all_totate.js
@@ -0,0 +1,55 @@
+let unitKyori;
+let maru = new Array();
+
+class Maru {
+	constructor(xx, yy) {
+    this.x = xx;
+    this.y = yy;
+  }
+}
+
+function setup(){
+  let unitYokoKazu = 10;
+  let yokoInterval = screenWidth / (unitYokoKazu - 1);
+
+  let tateInterval = yokoInterval * Math.sin(Math.PI / 3);
+  let unitTateKazu = Math.ceil(screenHeight / tateInterval) + 1;
+  unitKyori = yokoInterval;
+  for (let i = 0; i < unitYokoKazu * unitTateKazu; ++i) {
+    let tateNum = parseInt(i / unitYokoKazu);
+    let yokoNum = i % unitYokoKazu;
+    let x = yokoInterval * yokoNum;
+    let y = tateInterval * tateNum;
+    if(tateNum % 2 == 0)  x -= yokoInterval / 2;
+    maru[i] = new Maru(x, y);
+  }
+}
+
+function loop(){
+  ctx.clearRect(0, 0, screenWidth, screenHeight);
+  for(let i=0; i< maru.length; ++i){
+    if(yubiTouched) {
+      let xx = maru[i].x * Math.cos(Math.PI / 36) - maru[i].y * Math.sin(Math.PI / 36);
+      let yy = maru[i].x * Math.sin(Math.PI / 36) + maru[i].y * Math.cos(Math.PI / 36);
+      maru[i].x = xx;
+      maru[i].y = yy;
+    }
+    let hankei = unitKyori / 4;
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(maru[i].x, maru[i].y, hankei, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
+}
+
+function touchStart(){
+
+}
+
+function touchMove(){
+	
+}
+
+function touchEnd(){
+	
+}
