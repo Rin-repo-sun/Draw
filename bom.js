2a42fbc33217b3b639ae5f0b9b008b60e0ec3848
root <root@DESKTOP-MQEII7L>
2023-03-24 23:44:35 +0900

    edit bom

--- bom.js
+++ bom.js
@@ -1,8 +1,9 @@
-let V = [5, -3];
+let V = [5, -3, 4];
 
-let L = Math.sqrt(V[0] * V[0] + V[1] * V[1]);
+let L = Math.sqrt(V[0] * V[0] + V[1] * V[1] + V[2] * V[2]);
 
 V[0] /= L;
 V[1] /= L;
+V[2] /= L;
 
 console.log(V);
